e3cb3b21f9d0e7b165ef42798d900e85a1eb8a86
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-01-04 14:46:50 +0900

    [배환웅] 유저데이터 추가

---
+++ PointGame/Assets/Script/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData
+{
+    public int Index { get; private set; }
+    public string NickName = "";
+    public int Point { get; private set; }
+
+    public void SetData(int index, string nickName, int point)
+    {
+        Index = index;
+        NickName = nickName;
+        Point = point;
+    }
+
+    public void AddPoint(int point)
+    {
+        Point += point;
+    }
+}
