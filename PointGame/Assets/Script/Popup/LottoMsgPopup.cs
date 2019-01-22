437c96918769983322b5fed333127b1d9bf94819
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-01-22 15:52:03 +0900

    [배환웅] 로또 메세지 팝업 추가 선물상자 -> 푸쉬알림 변경

---
+++ PointGame/Assets/Script/Popup/LottoMsgPopup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LottoMsgPopup : Popup
+{
+    public Text Msg;
+    public Button OkButton;
+    private Action OkEndAction;
+    public Button CancelButton;
+
+    public class LottoMsgPopupData : PopupData
+    {
+        public string Msg;
+        public Action OkEndAction;
+
+        public LottoMsgPopupData(string msg, Action okEndAction = null)
+        {
+            PopupType = POPUP_TYPE.LOTTO_MSG;
+            Msg = msg;
+            OkEndAction = okEndAction;
+        }
+    }
+
+    private void Awake()
+    {
+        OkButton.onClick.AddListener(OnClickOk);
+        CancelButton.onClick.AddListener(OnClickCancel);
+    }
+
+    public override void SetData(PopupData data)
+    {
+        LottoMsgPopupData popupData = data as LottoMsgPopupData;
+        if (popupData == null)
+            return;
+
+        OkEndAction = popupData.OkEndAction;
+        SetMsg(popupData.Msg);
+    }
+
+    private void SetMsg(string msg)
+    {
+        Msg.text = msg;
+    }
+
+    public void OnClickOk()
+    {
+        CloseAction();
+        if (OkEndAction != null)
+            OkEndAction();
+    }
+
+    public void OnClickCancel()
+    {
+        CloseAction();
+    }
+}
