3573edb25d445b986657e72dc5f312db0371a4c0
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-01-26 17:29:48 +0900

    [배환웅] 데이터 수정

--- PointGame/Assets/Script/CommonData.cs
+++ PointGame/Assets/Script/CommonData.cs
@@ -16,6 +16,6 @@ public class CommonData : MonoBehaviour
 
     public static int LottoRefSeries = 1000000;
 
-    public static int InGameStepChangeStage = 2;
+    public static int InGameStepChangeStage = 5;
 
 }
