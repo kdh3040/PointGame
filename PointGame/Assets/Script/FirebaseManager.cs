df327a88e3951ae01b8eb7cc460a9577a6019a79
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2019-03-23 19:11:12 +0900

    [배환웅] 참가 신청 시간 버그 수정

--- PointGame/Assets/Script/FirebaseManager.cs
+++ PointGame/Assets/Script/FirebaseManager.cs
@@ -1713,7 +1713,7 @@ public class FirebaseManager : MonoBehaviour
                         var min = long.Parse(tempChild.Value.ToString());
                         var StartTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
                         StartTime = StartTime.AddMinutes(min);
-                        var EndTime = StartTime.AddMinutes(min);
+                        var EndTime = StartTime.AddMinutes(30);
 
                         TKManager.Instance.RPSEnterTimeList.Add(new KeyValuePair<long, long>(StartTime.Ticks, EndTime.Ticks));
                     }
